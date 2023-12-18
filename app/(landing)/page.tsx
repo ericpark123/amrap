42417c79f4a16f657933f1b88308f554fc423daf
Eric <ericpark123>
2023-12-18 14:25:18 -0800

    Designed landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,32 +3,41 @@
 import { useUser, SignInButton, SignUpButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import { Button } from '../(components)/ui/shadcn/button'
+import Image from 'next/image'
 
 const LandingPage = () => {
-  const { isSignedIn, isLoaded, user } = useUser()
+  const { isSignedIn, user } = useUser()
   
   return (
-    <div className='container relative'>
+    <div className='h-screen w-full flex items-center justify-center'>
       {
         isSignedIn ?
         <main className='flex items-center justify-center py-4'>
           <Link href="/home">Welcome {user.firstName}</Link>
         </main>:
         (
-          <div className="mt-4">   
-            <nav className= "flex justify-center items-center space-x-6 text-sm font-medium">
-              <div>
-              <Button variant={"ghost"}>
-                <SignInButton />
-              </Button>
+            <main className='rounded-lg border-solid border-2 py-10 px-10 border-background bg-white shadow-lg'>
+              <h1 className='flex justify-center'>
+                <Image
+                  src="/amrap_logo.png"
+                  priority
+                  height={0}
+                  width={0}
+                  style={{width:'300px', height: "auto" }}
+                  alt="AMRAP logo"
+                />
+              </h1>
+              <h2 className='flex justify-center text-black py-5 mt-10 tracking-tighter'>
+              </h2>
+              <div className='grid items-center justify-center'>
+                <SignInButton>
+                  <Button className='text-background shadow-sm rounded-full px-24 py-2 mt-3' variant={'default'}>Sign in with Clerk</Button>
+                </SignInButton>
+                <SignUpButton>
+                  <Button className='bg-white border-2 border-foreground text-background shadow-sm rounded-full px-24 py-2 mt-3' variant={'default'}>Sign up</Button>
+                </SignUpButton>
               </div>
-              <div>
-              <Button variant={"ghost"}>
-                <SignUpButton />
-              </Button>
-              </div>
-            </nav>    
-          </div>
+            </main>    
         )    
       }
     </div>   
